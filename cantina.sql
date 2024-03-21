03ff02ac20950eb9e9fc2700f2ea27087e87bfb2
Jonas Otávio B. de Oliveira <120534373+Jonas01101111@users.noreply.github.com>
2024-03-21 15:59:06 -0300

    Add files via upload

--- cantina.sql
+++ cantina.sql
@@ -1,12 +1,21 @@
 create database if not exists dbcantina;
 use dbcantina;
-#1
+
+
+create table if not exists estado (
+id int auto_increment,
+estado varchar(25),
+UF char(2)
+);
+# EXTRA1
+
+
 create table if not exists categoria (
 id int auto_increment,
 descricao varchar(30),
 primary key(id)
 );
-#
+#1
 create table if not exists produto(
 id int auto_increment,
 nome varchar(30),
@@ -18,15 +27,23 @@ primary key(id),
 foreign key(id_categoria) references categoria(id)
 );
 #2
-create table if not exists enderececo(
+create table if not exists endereco(
 id int auto_increment,
-,
+cidade varchar(35),
+bairro varchar(50),
+id_estado int,
+cep varchar(8),
+logradouro varchar(60),
+foreign key(id_estado) references estado(id),
 primary key(id)
 );
 #3
-create table if not exists (
+create table if not exists fornecedor(
 id int auto_increment,
-,
+nome varchar(40),
+email varchar(70),
+telefone char(14),
+cnpj char(14),
 primary key(id)
 );
 #4
@@ -90,4 +107,5 @@ id int auto_increment,
 ,
 primary key(id)
 );
+
 
