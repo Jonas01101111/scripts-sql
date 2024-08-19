16110db9360a06a2cc3f97ff887c0be9689b63da
Jonas Otávio B. de Oliveira <120534373+Jonas01101111@users.noreply.github.com>
2024-08-19 15:57:30 -0300

    Add files via upload

---
+++ Aula Update.sql
@@ -0,0 +1,22 @@
+select * from alunos ;
+select * from cidades;
+update alunos set data_nascimento = "2013-01-31", id_cidade = 1 where id=1;
+
+#1 
+update alunos set id_cidade=6 where nome like "%Coriolano%";
+#2
+update alunos set bairro = "Vila Ana Maria" where pai like "%Ribeirinho%";
+#3
+update alunos set Numero = 530 where nome like "%Zarolha%";
+#4
+update alunos set mae = "Miquelina das Onças" where nome like "Manoel das Onças";
+#5
+update alunos set cpf = "255.754.445-79" where nome like "Maria Machadão";
+#6 
+update alunos set pai = "Juvenal das Couves" where nome like "Doroteia";
+#7
+update alunos set where;
+#8
+update alunos set where;
+#9
+update alunos set where;
